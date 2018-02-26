427f2d696bda3fae2412caf9d74015cd3538e25c
pazuzu111 <pazuzublue111@gmail.com>
2018-02-26 09:22:19 -0500

    created delay fucntion for in progress fucntionality, created odd fucntion to grab the state of the odd, & rendered props

--- src/Ant.js
+++ src/Ant.js
@@ -1,27 +1,27 @@
-// import React, { Component } from 'react';
-// import './App.css';
+import React, { Component } from 'react';
+import './App.css';
 
-// export default class Ant extends Component {
-//     constructor(props) {
-//         super(props);
+export default class Ant extends Component {
+    constructor(props) {
+        super(props);
 
-//         //initialize state
-//         this.state = ({
-//             status: 'calculate?',
-//             clicked: false,
-//             odds: 0,
-//         });
-//     }
+        //initialize state
+        this.state = ({
+            status: 'calculate?',
+            clicked: false,
+            odds: 0,
+        });
+    }
 
-//     //on mount set ref
-//     componentDidMount() {
-//         this.props.onRef(this)
-//     }
+    //on mount set ref
+    componentDidMount() {
+        this.props.onRef(this)
+    }
 
-//     //grab odds state
-//     odd = () => {
-//         return this.state.odds
-//     }
+    //grab odds state
+    odd = () => {
+        return this.state.odds
+    }
 
     //calculate odds of an Ant
     getOdds = () => {
@@ -58,45 +58,45 @@
         })
     }
 
-//     //onclick flip state of status to in progress
-//     delay = () => {
+    //onclick flip state of status to in progress
+    delay = () => {
 
-//         this.setState({
-//             clicked: true,
-//             status: 'in progress'
-//         })
+        this.setState({
+            clicked: true,
+            status: 'in progress'
+        })
 
-//         //start calculating odds
-//         this.getOdds()
-//     }
+        //start calculating odds
+        this.getOdds()
+    }
 
-//   render() {
-//     return (
-//         <div className="ant">
-//             <div className="flip-container">
-//                 <div className="flipper">
-//                     <div className="front">
-//                         <div className="left">
-//                             <p>{this.props.ant.name}</p>
-//                             <h6>{this.props.ant.length} in.
-//                             <span> &#8226; </span>
-//                             {this.props.ant.weight} oz.
-//                             <span> &#8226; </span>
-//                             {this.props.ant.color}</h6>
-//                         </div>
-//                         <div className="right">
-//                             <p>{this.state.odds}%</p>
-//                             <p>{this.state.status}</p>
-//                         </div>
-//                     </div>
-//                     <div className="back">
-//                         <div className="description">
-//                             <h2>HIRE ME  😊</h2>
-//                         </div>
-//                     </div>
-//                 </div>
-//             </div>
-//         </div>
-//     );
-//   }
-// }
+  render() {
+    return (
+        <div className="ant">
+            <div className="flip-container">
+                <div className="flipper">
+                    <div className="front">
+                        <div className="left">
+                            <p>{this.props.ant.name}</p>
+                            <h6>{this.props.ant.length} in.
+                            <span> &#8226; </span>
+                            {this.props.ant.weight} oz.
+                            <span> &#8226; </span>
+                            {this.props.ant.color}</h6>
+                        </div>
+                        <div className="right">
+                            <p>{this.state.odds}%</p>
+                            <p>{this.state.status}</p>
+                        </div>
+                    </div>
+                    <div className="back">
+                        <div className="description">
+                            <h2>HIRE ME  😊</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+  }
+}
